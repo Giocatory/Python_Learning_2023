e6fc88c7423a5a9abe4edc1e96c34365fa61f377
Giocatory <nivolyas@gmail.com>
2023-01-02 11:36:06 +0300

    02.01.2023

--- По книге Бена Бейдера/Basic_List_methods.py
+++ По книге Бена Бейдера/Basic_List_methods.py
@@ -1,3 +1,5 @@
+import copy
+
 colors = ["red", "green", "blue"]
 
 # Вставить элемент в указанное место
@@ -18,3 +20,21 @@ print(*colors, sep=', ')  # red, green, blue, violet
 
 colors.pop()
 print(*colors, sep=', ')  # red, green, blue
+
+# добавление нескольких элементов
+colors.extend(["black", "white", "gray"])
+print(*colors, sep=', ')  # red, green, blue, black, white, gray
+
+# Копирование списков
+# 1 slice
+new_colors = colors[:]  # Поверхностное копирование
+print(new_colors)  # ['red', 'green', 'blue', 'black', 'white', 'gray']
+# 2 import copy
+new_colors = copy.deepcopy(colors)
+print(new_colors)  # ['red', 'green', 'blue', 'black', 'white', 'gray']
+
+# Сортировка
+colors.sort()
+print(colors)  # ['black', 'blue', 'gray', 'green', 'red', 'white']
+colors.sort(key=len)
+print(colors)  # ['red', 'blue', 'gray', 'black', 'green', 'white']
