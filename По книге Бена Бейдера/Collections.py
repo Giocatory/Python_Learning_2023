cf12101c84282819f215fcc700d0c6bfa7de5dff
Giocatory <nivolyas@gmail.com>
2023-01-02 06:40:36 +0300

    02.01.2023

--- По книге Бена Бейдера/Collections.py
+++ По книге Бена Бейдера/Collections.py
@@ -3,3 +3,10 @@ print(type(a))  # <class 'list'>
 print(type(b))  # <class 'dict'>
 print(type(c))  # <class 'tuple'>
 print(type(d))  # <class 'set'>
+
+
+def return_tuple():
+    return 1, 2, 3
+
+
+print(type(return_tuple()))  # <class 'tuple'>
