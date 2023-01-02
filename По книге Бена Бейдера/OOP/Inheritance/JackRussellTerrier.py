39a8b4d3bba2c930ac903c1264b01ea8cd1d6d0f
Giocatory <nivolyas@gmail.com>
2023-01-02 16:03:57 +0300

    02.01.2023

--- По книге Бена Бейдера/OOP/Inheritance/JackRussellTerrier.py
+++ По книге Бена Бейдера/OOP/Inheritance/JackRussellTerrier.py
@@ -4,7 +4,7 @@ from Class_Dog import Dog
 class JackRussellTerrier(Dog):
 
     def __init__(self, name, age):
-        Dog.__init__(self, name, age)
+        super().__init__(name, age)
 
     def speak(self):
         return f"{self._get_name()} say GRRRRR"
