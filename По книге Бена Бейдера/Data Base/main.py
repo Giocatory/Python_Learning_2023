b25cc1f06e69adbccaeab707e399959e566ecc3e
Giocatory <nivolyas@gmail.com>
2023-01-04 09:41:29 +0300

    04.01.2023

--- По книге Бена Бейдера/Data Base/main.py
+++ По книге Бена Бейдера/Data Base/main.py
@@ -2,7 +2,21 @@ import sqlite3
 
 
 def main():
-    connection = sqlite3.connect("test_database.db")
+    create_table = """CREATE TABLE People(FirstName TEXT, LastName TEXT, Age INT);
+    INSERT INTO People VALUES("Mikhail", "Derkunov", 34);"""
+
+    # test_database.db создастся, если его нет
+    with sqlite3.connect("test_database.db") as connection:
+        cursor = connection.cursor()
+        query = "SELECT datetime('now', 'localtime');"
+        result = cursor.execute(query)
+        row = result.fetchone()
+        time = row[0]
+        print(time)  # 2023-01-04 09:21:34
+
+        cursor.executescript(create_table)
+        # cursor.execute(query) - только для одной команды
+        # cursor.executescript(query's) - для нескольких команд
 
 
 if __name__ == '__main__':
