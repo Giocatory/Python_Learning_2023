297a6e38f538d7925523ef5f958f899a5f7d0841
Giocatory <nivolyas@gmail.com>
2023-01-04 16:13:25 +0300

    04.01.2023

--- По книге Бена Бейдера/OOP/Basic_class/Class_Dog.py
+++ По книге Бена Бейдера/OOP/Basic_class/Class_Dog.py
@@ -1,16 +1,29 @@
 class Dog:
-    dogs_created = 0
+    __dogs_created = 0
 
     def __init__(self, name="No-name", age="No-age"):
         self.__name = name
         self.__age = age
-        Dog.dogs_created += 1
+        Dog.__dogs_created += 1
+
+    @classmethod
+    def how_many_dogs_created(cls):
+        return cls.__dogs_created
 
     def __str__(self):
         return f"Dog: {self.__name} and {self.__age} years"
 
-    def get_name(self):
+    # def get_name(self):
+    #     return self.__name
+    @property
+    def name(self):
         return self.__name
 
-    def get_age(self):
+    @property
+    def age(self):
         return self.__age
+
+    @age.setter
+    def age(self, value):
+        if 0 < value < 30:
+            self.__age = value
--- По книге Бена Бейдера/OOP/Basic_class/Program.py
+++ По книге Бена Бейдера/OOP/Basic_class/Program.py
@@ -4,13 +4,15 @@ first_dog = Dog("Бобик", 3)
 second_dog = Dog("Шарик", 4)
 
 print(first_dog)  # Dog: Бобик and 3 years
-print(first_dog.get_name())  # Бобик
+print(first_dog.name)  # Бобик
+first_dog.age = 5
+print(first_dog.age)  # 5
 
 
 print(second_dog)  # Dog: Шарик and 4 years
-print(second_dog.get_age())  # 4
+print(second_dog.age)  # 4
 
-print(Dog.dogs_created)  # 2
+print(Dog.how_many_dogs_created())  # 2
 
 # полной инкапсуляции нет
 print(first_dog._Dog__age)  # 3, но так делать не рекомендуют
